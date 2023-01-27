7a495e9e862503647bbf507e8e81c49739647e18
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-27 08:59:48 -0500

    Update post-production.sh

--- post-production.sh
+++ post-production.sh
@@ -1,3 +1,6 @@
+# Global variables.
+PATH_TO_MICRO_SD_CARD=$()
+
 # Remove all the temp .lrf files.
 
 # Make sure all the video files are valid.
@@ -5,3 +8,9 @@
 # Combine all the videos into a single video.
 
 # Combine all the .srt files into one.
+
+# Export the content out of here.
+
+# Remove the content from the SD card.
+
+# Upload the content to youtube.
