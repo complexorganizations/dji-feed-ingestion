de0d654f39389b3f50c535ee99911f0816a2cf48
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-27 10:35:02 -0500

    Update post-production.sh

--- post-production.sh
+++ post-production.sh
@@ -3,7 +3,9 @@ PATH_TO_MICRO_SD_CARD=$()
 
 # Remove all the temp .lrf files.
 
-# Make sure all the video files are valid.
+# Zip all the files and upload them to S3.
+
+# Unzip all the videos and make sure all the video files are valid.
 
 # Combine all the videos into a single video.
 
