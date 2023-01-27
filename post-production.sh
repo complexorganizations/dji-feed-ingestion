bc37b1c6456d0010cc11b183f33aaddad9489ce2
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-27 10:47:25 -0500

    Update post-production.sh

--- post-production.sh
+++ post-production.sh
@@ -1,11 +1,16 @@
 # Global variables.
 PATH_TO_MICRO_SD_CARD=$()
+RANDOM_DRONE_RUN_ID=$(openssl rand -hex 6)
+DRONE_VIDEO_ZIP_NAME="${RANDOM_DRONE_RUN_ID}.zip"
 
 # Remove all the temp .lrf files.
+rm -f 100MEDIA/*.LRF
 
 # Zip all the files and upload them to S3.
+zip -r ${DRONE_VIDEO_ZIP_NAME} 100MEDIA/
 
 # Unzip all the videos and make sure all the video files are valid.
+unzip ${DRONE_VIDEO_ZIP_NAME} -d ${RANDOM_DRONE_RUN_ID}
 
 # Combine all the videos into a single video.
 
