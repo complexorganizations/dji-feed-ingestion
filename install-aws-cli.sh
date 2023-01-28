d99f1cf8c4e23b5b609d359f1ae9a985f66ffe4a
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-27 22:17:21 -0500

    Delete install-aws-cli.sh

--- install-aws-cli.sh
+++
@@ -1,6 +0,0 @@
-apt-get install curl unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY }}
-aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }}
