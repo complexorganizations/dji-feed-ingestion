a71dd559f6456c053c717fb2cc95bb706f885816
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-28 08:46:52 -0500

    Update rtsp-to-kensis-live.sh

--- rtsp-to-kensis-live.sh
+++ rtsp-to-kensis-live.sh
@@ -1,8 +1,9 @@
 sudo apt-get install pkg-config cmake m4 git build-essential -y
 sudo apt-get install libssl-dev libcurl4-openssl-dev liblog4cplus-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools -y
+cd /etc/
 git clone https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git
-mkdir -p amazon-kinesis-video-streams-producer-sdk-cpp/build
-cd amazon-kinesis-video-streams-producer-sdk-cpp/build
+mkdir -p /etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
+cd /etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
 cmake -DBUILD_GSTREAMER_PLUGIN=TRUE ..
 make
 export GST_PLUGIN_PATH=`pwd`/build
