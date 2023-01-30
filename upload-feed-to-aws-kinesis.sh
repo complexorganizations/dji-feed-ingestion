91a5a5c30f40e0f3440c7a3a0309b29dfd30979c
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-30 13:51:53 -0500

    Update upload-feed-to-aws-kinesis.sh

--- upload-feed-to-aws-kinesis.sh
+++ upload-feed-to-aws-kinesis.sh
@@ -1,4 +1,4 @@
-sudo apt-get install pkg-config cmake m4 git build-essential -y
+sudo apt-get install pkg-config cmake m4 git build-essential jq -y
 sudo apt-get install libssl-dev libcurl4-openssl-dev liblog4cplus-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools -y
 cd /etc/
 git clone https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git
@@ -9,4 +9,4 @@ make
 echo "export GST_PLUGIN_PATH=/etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
 export LD_LIBRARY_PATH=/etc/amazon-kinesis-video-streams-producer-sdk-cpp/open-source/local/lib" >> ~/.profile
 source ~/.profile
-AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-0 rtsp://admin:password@157.230.180.160:8554/test
+AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-0 rtsp://admin:password@$(curl --ipv4 --connect-timeout 5 --tlsv1.3 --silent 'https://api.ipengine.dev' | jq -r '.network.ip'):8554/drone_0
