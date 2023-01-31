d32fc5acd0a03cee1e7539de5ca861da38b00b3e
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-01-30 20:12:56 -0500

    Update upload-feed-to-aws-kinesis.sh

--- upload-feed-to-aws-kinesis.sh
+++ upload-feed-to-aws-kinesis.sh
@@ -1,12 +1,107 @@
-sudo apt-get install pkg-config cmake m4 git build-essential jq -y
-sudo apt-get install libssl-dev libcurl4-openssl-dev liblog4cplus-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools -y
-cd /etc/
-git clone https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git
-mkdir -p /etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
-cd /etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
-cmake -DBUILD_GSTREAMER_PLUGIN=TRUE ..
-make
-echo "export GST_PLUGIN_PATH=/etc/amazon-kinesis-video-streams-producer-sdk-cpp/build
-export LD_LIBRARY_PATH=/etc/amazon-kinesis-video-streams-producer-sdk-cpp/open-source/local/lib" >> ~/.profile
-source ~/.profile
-AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-0 rtsp://admin:password@$(curl --ipv4 --connect-timeout 5 --tlsv1.3 --silent 'https://api.ipengine.dev' | jq -r '.network.ip'):8554/drone_0
+#!/usr/bin/env bash
+# https://github.com/complexorganizations/wireguard-manager
+
+# Require script to be run as root
+function super-user-check() {
+    # This code checks to see if the script is running with root privileges.
+    # If it is not, it will exit with an error message.
+    if [ "${EUID}" -ne 0 ]; then
+        echo "Error: You need to run this script as administrator."
+        exit
+    fi
+}
+
+# Check for root
+super-user-check
+
+# Get the current system information
+function system-information() {
+    # CURRENT_DISTRO is the ID of the current system
+    # CURRENT_DISTRO_VERSION is the VERSION_ID of the current system
+    # CURRENT_DISTRO_MAJOR_VERSION is the major version of the current system (e.g. "16" for Ubuntu 16.04)
+    if [ -f /etc/os-release ]; then
+        # shellcheck source=/dev/null
+        source /etc/os-release
+        CURRENT_DISTRO=${ID}
+        CURRENT_DISTRO_VERSION=${VERSION_ID}
+        CURRENT_DISTRO_MAJOR_VERSION=$(echo "${CURRENT_DISTRO_VERSION}" | cut --delimiter="." --fields=1)
+    fi
+}
+
+# Get the current system information
+system-information
+
+# Pre-Checks system requirements
+function installing-system-requirements() {
+    if { [ "${CURRENT_DISTRO}" == "ubuntu" ] || [ "${CURRENT_DISTRO}" == "debian" ] || [ "${CURRENT_DISTRO}" == "raspbian" ] || [ "${CURRENT_DISTRO}" == "pop" ] || [ "${CURRENT_DISTRO}" == "kali" ] || [ "${CURRENT_DISTRO}" == "linuxmint" ] || [ "${CURRENT_DISTRO}" == "neon" ] || [ "${CURRENT_DISTRO}" == "fedora" ] || [ "${CURRENT_DISTRO}" == "centos" ] || [ "${CURRENT_DISTRO}" == "rhel" ] || [ "${CURRENT_DISTRO}" == "almalinux" ] || [ "${CURRENT_DISTRO}" == "rocky" ] || [ "${CURRENT_DISTRO}" == "arch" ] || [ "${CURRENT_DISTRO}" == "archarm" ] || [ "${CURRENT_DISTRO}" == "manjaro" ] || [ "${CURRENT_DISTRO}" == "alpine" ] || [ "${CURRENT_DISTRO}" == "freebsd" ] || [ "${CURRENT_DISTRO}" == "ol" ]; }; then
+        if { [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v cut)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v ip)" ] || [ ! -x "$(command -v lsof)" ] || [ ! -x "$(command -v cron)" ] || [ ! -x "$(command -v awk)" ] || [ ! -x "$(command -v ps)" ] || [ ! -x "$(command -v grep)" ] || [ ! -x "$(command -v qrencode)" ] || [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v zip)" ] || [ ! -x "$(command -v unzip)" ] || [ ! -x "$(command -v openssl)" ] || [ ! -x "$(command -v nft)" ] || [ ! -x "$(command -v ifup)" ] || [ ! -x "$(command -v chattr)" ] || [ ! -x "$(command -v gpg)" ] || [ ! -x "$(command -v systemd-detect-virt)" ]; }; then
+            if { [ "${CURRENT_DISTRO}" == "ubuntu" ] || [ "${CURRENT_DISTRO}" == "debian" ] || [ "${CURRENT_DISTRO}" == "raspbian" ] || [ "${CURRENT_DISTRO}" == "pop" ] || [ "${CURRENT_DISTRO}" == "kali" ] || [ "${CURRENT_DISTRO}" == "linuxmint" ] || [ "${CURRENT_DISTRO}" == "neon" ]; }; then
+                apt-get update
+                apt-get install pkg-config cmake m4 git build-essential jq libssl-dev libcurl4-openssl-dev liblog4cplus-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools -y
+            elif { [ "${CURRENT_DISTRO}" == "fedora" ] || [ "${CURRENT_DISTRO}" == "centos" ] || [ "${CURRENT_DISTRO}" == "rhel" ] || [ "${CURRENT_DISTRO}" == "almalinux" ] || [ "${CURRENT_DISTRO}" == "rocky" ]; }; then
+                yum check-update
+            elif { [ "${CURRENT_DISTRO}" == "arch" ] || [ "${CURRENT_DISTRO}" == "archarm" ] || [ "${CURRENT_DISTRO}" == "manjaro" ]; }; then
+                pacman -Sy --noconfirm archlinux-keyring
+            elif [ "${CURRENT_DISTRO}" == "alpine" ]; then
+                apk update
+            elif [ "${CURRENT_DISTRO}" == "freebsd" ]; then
+                pkg update
+            elif [ "${CURRENT_DISTRO}" == "ol" ]; then
+                yum check-update
+            fi
+        fi
+    else
+        echo "Error: ${CURRENT_DISTRO} ${CURRENT_DISTRO_VERSION} is not supported."
+        exit
+    fi
+}
+
+# check for requirements
+installing-system-requirements
+
+# Global variables
+AMAZON_KINESIS_VIDEO_STREAMS_GIT_PATH="https://github.com/awslabs/amazon-kinesis-video-streams-producer-sdk-cpp.git"
+AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_PATH="/etc/amazon-kinesis-video-streams-producer-sdk-cpp"
+AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_BUILD_PATH="${AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_PATH}/build"
+AMAZON_KINESIS_VIDEO_STREAMS_OPEN_SOURCE_LOCAL_LIB_PATH="${AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_PATH}/open-source/local/lib"
+# RTSP Paths
+RTSP_SERVER_ZERO="rtsp://admin:password@localhost:8554/zero"
+RTSP_SERVER_ONE="rtsp://admin:password@localhost:8554/one"
+RTSP_SERVER_TWO="rtsp://admin:password@localhost:8554/two"
+RTSP_SERVER_THREE="rtsp://admin:password@localhost:8554/three"
+
+# Build the application.
+function build-kensis-application() {
+    cd /etc/
+    git clone ${AMAZON_KINESIS_VIDEO_STREAMS_GIT_PATH}
+    mkdir -p ${AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_BUILD_PATH}
+    cd ${AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_BUILD_PATH}
+    cmake -DBUILD_GSTREAMER_PLUGIN=TRUE ..
+    make
+    # Add the path to the .profile file so that it can be used in the future
+    echo "export GST_PLUGIN_PATH=${AMAZON_KINESIS_VIDEO_STREAMS_PRODUCER_PATH}
+export LD_LIBRARY_PATH=${AMAZON_KINESIS_VIDEO_STREAMS_OPEN_SOURCE_LOCAL_LIB_PATH}" >>~/.profile
+    source ~/.profile
+}
+
+# Build the application.
+build-kensis-application
+
+# Check the RTSP server status
+function check-rtsp-server-status() {
+    # Check if a given RTSP server is alive and if it is than stream it
+    if [ "$(ffprobe -v quiet -print_format json -show_streams ${RTSP_SERVER_ZERO} | wc -m)" ] >= 1000; then
+        AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-0 ${RTSP_SERVER_ZERO}
+    elif [ "$(ffprobe -v quiet -print_format json -show_streams ${RTSP_SERVER_ONE} | wc -m)" ] >= 1000; then
+        AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-1 ${RTSP_SERVER_ONE}
+    elif [ "$(ffprobe -v quiet -print_format json -show_streams ${RTSP_SERVER_TWO} | wc -m)" ] >= 1000; then
+        AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-2 ${RTSP_SERVER_TWO}
+    elif [ "$(ffprobe -v quiet -print_format json -show_streams ${RTSP_SERVER_THREE} | wc -m)" ] >= 1000; then
+        AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_DEFAULT_REGION=us-east-1 ./kvs_gstreamer_sample dji-stream-3 ${RTSP_SERVER_THREE}
+    else
+        exit
+    fi
+}
+
+# Check if the RTSP server is alive and if it is than stream it
+check-rtsp-server-status
