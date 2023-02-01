c71df3e537a9a933a68258d3256fd19b5e7f568d
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-01 00:57:49 -0500

    Update post-production.sh

--- post-production.sh
+++ post-production.sh
@@ -6,26 +6,24 @@ DRONE_VIDEO_ZIP_NAME="${RANDOM_DRONE_RUN_ID}.zip"
 # Remove all the temp .lrf files.
 rm -f 100MEDIA/*.LRF
 
+# Zip all the files.
+zip -r ${DRONE_VIDEO_ZIP_NAME} 100MEDIA/
+
+# Upload all the files to a given service like S3
+aws s3 cp ${DRONE_VIDEO_ZIP_NAME} s3://mybucket/myfolder
+
+# Remove the content from the SD card.
+rm -r 100MEDIA/*
+
+# Using a serivce like lamda function unzip the files.
+# Unzip all the videos and make sure all the video files are valid.
+$ unzip ${DRONE_VIDEO_ZIP_NAME} -d ${RANDOM_DRONE_RUN_ID}
+
 # Validate all the recordings are good.
 ffmpeg -v error -i first_input.mp4 -f null - 2 >> error.log
 
 # Combine all the recordings into one.
 ffmpeg -i concat:"first_input.mp4|second_input.mp4" output.mp4
 
-# Zip all the files.
-zip -r ${DRONE_VIDEO_ZIP_NAME} 100MEDIA/
-
-# Upload all the files to a given service like S3
-
-# Unzip all the videos and make sure all the video files are valid.
-unzip ${DRONE_VIDEO_ZIP_NAME} -d ${RANDOM_DRONE_RUN_ID}
-
-# Combine all the videos into a single video.
-
 # Combine all the .srt files into one.
-
-# Export the content out of here.
-
-# Remove the content from the SD card.
-
-# Upload the content to youtube.
+cat *.SRT >> all.srt
