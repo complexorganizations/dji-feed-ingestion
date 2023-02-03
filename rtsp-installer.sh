c6f89e62757ff34e19a1f055296047fbd3208f27
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-02 19:41:30 -0500

    Update rtsp-installer.sh

--- rtsp-installer.sh
+++ rtsp-installer.sh
@@ -97,6 +97,7 @@ RTSP_SIMPLE_SERVER_PATH="/etc/rtsp-simple-server"
 RTSP_SIMPLE_SERVER_CONFIG="${RTSP_SIMPLE_SERVER_PATH}/rtsp-simple-server.yml"
 RTSP_SIMPLE_SERVICE_APPLICATION="${RTSP_SIMPLE_SERVER_PATH}/rtsp-simple-server"
 RTSP_SIMPLE_SERVER_SERVICE="/etc/systemd/system/rtsp-simple-server.service"
+RTSP_CONFIG_FILE_GITHUB_URL="https://raw.githubusercontent.com/complexorganizations/dji-feed-analysis/main/rtsp-simple-server.yml"
 LATEST_RELEASES=$(curl -s https://api.github.com/repos/aler9/rtsp-simple-server/releases | grep browser_download_url | cut -d'"' -f4 | grep $(dpkg --print-architecture) | grep linux | head -n3 | cut --delimiter="/" --fields=9 | cut --delimiter="_" --fields=2)
 
 # Check if the rtsp-simple-server directory dosent exists
@@ -136,6 +137,7 @@ if [ ! -d "${RTSP_SIMPLE_SERVER_PATH}" ]; then
     curl -L "${DOWNLOAD_URL}" -o /tmp/${FILE_NAME}
     mkdir -p ${RTSP_SIMPLE_SERVER_PATH}
     tar -xvf /tmp/${FILE_NAME} -C ${RTSP_SIMPLE_SERVER_PATH}
+    curl ${RTSP_CONFIG_FILE_GITHUB_URL} -o ${RTSP_SIMPLE_SERVER_CONFIG}
   }
 
   # Download the latest release
