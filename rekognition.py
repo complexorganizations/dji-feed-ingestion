8c24eb1d98406a68ace974067056750cb7961133
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-09 12:26:31 -0500

    Update rekognition.py

--- rekognition.py
+++ rekognition.py
@@ -6,6 +6,11 @@ def get_bucket_name_from_s3_uri(s3_uri):
     return s3_uri.split("/")[2]
 
 
+# Get the path from a s3 URI
+def get_path_from_s3_uri(s3_uri):
+    return "/".join(s3_uri.split("/")[3:])
+
+
 # Get the response from Rekognition
 def get_response(s3URI):
     rekognition = boto3.client("rekognition", region_name="us-east-1")
@@ -13,7 +18,7 @@ def get_response(s3URI):
         Image={
             "S3Object": {
                 "Bucket": get_bucket_name_from_s3_uri(s3URI),
-                "Name": s3URI
+                "Name": get_path_from_s3_uri(s3URI),
             }
         }
     )
@@ -22,7 +27,8 @@ def get_response(s3URI):
 
 def main():
     # Get the response from Rekognition
-    response = get_response("s3://dji-live-stream-feed-data-0/media/dji-stream-0//113669145637_dji-stream-0_1675351204694_f8953f18-b68f-469e-bd16-7b972f549c62.jpg")
+    response = get_response(
+        "s3://dji-live-stream-feed-data-0/media/dji-stream-0//113669145637_dji-stream-0_1675350588904_b574d515-c67b-49af-8c74-48fd302bd3c0.jpg")
     print(response)
 
 
