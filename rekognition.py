ae6c631b5e56c6e3144cb2afc90075df05e2b0d9
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-09 11:45:54 -0500

    Update rekognition.py

--- rekognition.py
+++ rekognition.py
@@ -1,12 +1,5 @@
 import boto3
 
-def init():
-    s3 = boto3.resource("s3")
-    rekognition = boto3.client("rekognition")
-
-
-init()
-
 def main():
     # Get the response from Rekognition
     response = get_response("bucket", "key")
@@ -16,6 +9,8 @@ main()
 
 # Get the response from Rekognition
 def get_response(bucket, key):
+    s3 = boto3.resource("s3")
+    rekognition = boto3.client("rekognition")
     response = rekognition.detect_labels(
         Image={
             "S3Object": {
