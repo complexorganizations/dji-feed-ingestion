cfbe019dcb0eb7df4eeebe43d0e6c12f48a2ca18
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-11 07:42:35 -0500

    Update upload-feed-into-vertex-ai.sh

--- gcp/upload-feed-into-vertex-ai.sh
+++ gcp/upload-feed-into-vertex-ai.sh
@@ -1,7 +1,8 @@
 sudo apt-get install apt-transport-https ca-certificates gnupg -y
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-cli -y
+sudo apt-get update
+sudo apt-get install google-cloud-cli -y
 gcloud auth application-default login
 gcloud services enable visionai.googleapis.com
 wget https://github.com/google/visionai/releases/download/v0.0.4/visionai_0.0-4_amd64.deb
