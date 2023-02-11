d05651deafa93635b4037357eaa766abbcd99400
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-11 08:40:54 -0500

    Update upload-feed-to-aws-ivs.sh

--- aws/upload-feed-to-aws-ivs.sh
+++ aws/upload-feed-to-aws-ivs.sh
@@ -3,12 +3,11 @@ RTSP_CONNECTION_STRING="rtsp://Administrator:Password@localhost:8554/drone_0"
 RTSP_VIDEO_PATH="/etc/rtsp-simple-server/main.ts"
 AWS_IVS_CONNECTION_URL=""
 
-# Always record the feed; even thought it might be offline since we don't know when it will come online.
+# Record the stream live.
 ffmpeg -i ${RTSP_CONNECTION_STRING} -c copy -f segment -strftime 1 -segment_time 3600 -segment_format mpegts ${RTSP_VIDEO_PATH}
 
-# Check the feed is online every minute; and if its online than feed it into aws.
-
-# Prevent double feed into aws; only feed the data into aws once.
-
-# Check if a given feed is alive.
+# Push the feed into aws via the recording.
 ffmpeg -re -stream_loop -1 -i ${RTSP_VIDEO_PATH} -r 30 -c:v libx264 -pix_fmt yuv420p -profile:v main -preset veryfast -x264opts "nal-hrd=cbr:no-scenecut" -minrate 3000 -maxrate 3000 -g 60 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv ${AWS_IVS_CONNECTION_URL}
+
+# Push the feed into aws ivs via the rtmps
+ffmpeg -re -stream_loop -1 -i ${RTSP_CONNECTION_STRING} -c copy -f flv ${AWS_IVS_CONNECTION_URL}
