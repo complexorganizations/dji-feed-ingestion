009001ddfe09391a442992622fffe91d286781b6
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-12 18:45:38 -0500

    Create helper_functions.go

---
+++ csp-connector/helper_functions.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
+
+/*
+It checks if the file exists
+If the file exists, it returns true
+If the file does not exist, it returns false
+*/
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
+
+/*
+It takes in a path and content to write to that file.
+It uses the os.WriteFile function to write the content to that file.
+It checks for errors and logs them.
+*/
+func writeToFile(path string, content []byte) {
+	err := os.WriteFile(path, content, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Read a file and return the contents
+func readAFileAsString(path string) string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(content)
+}
+
+
+// Get the sha 256 of a file and return it as a string
+func sha256OfFile(filePath string) string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	hash := sha512.New()
+	io.Copy(hash, file)
+	err = file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+// Save all the errors in a single given path.
+func saveAllErrors(errors error, path string) {
+	filePath, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetOutput(filePath)
+	log.Println(errors)
+	err = filePath.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// exitTheApplication prints the message to the log and exits the application
+func exitTheApplication(message string) {
+	log.Fatalln(message)
+}
+
+// Check if the application is installed and in path
+func commandExists(application string) bool {
+	_, err := exec.LookPath(application)
+	return err == nil
+}
