91ba637dfcb01b29a6cdcb1715c0f77c3a200eac
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-12 18:45:08 -0500

    Create init.go

---
+++ csp-connector/init.go
@@ -0,0 +1,22 @@
+package main
+
+var (
+	applicationConfigFile = "config.json"
+)
+
+func init() {
+	// Check if the config file exists in the current directory
+	if !fileExists(applicationConfigFile) {
+		// Write a config file in the current directory if it doesn't exist
+		writeToFile(applicationConfigFile, []byte(``))
+	}
+	// Check if the config file has not been modified
+	if sha256OfFile(applicationConfigFile) == "d41d8cd98f00b204e9800998ecf8427e" {
+		// The file has not been modified
+		exitTheApplication("The config file has not been modified. Please modify it and try again.")
+	}
+	// Check if the required application are present in the system
+	if commandExists("git") == false {
+		exitTheApplication("Git is not installed in your system. Please install it and try again.")
+	}
+}
