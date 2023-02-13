714c09da5f95fadb58db25466109714b4dac0a4f
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-02-13 05:28:25 -0500

    Update init.go

--- csp-connector/init.go
+++ csp-connector/init.go
@@ -8,8 +8,11 @@ var (
 func init() {
 	// Check if the config file exists in the current directory
 	if !fileExists(applicationConfigFile) {
+		// Deafult config string.
+		defaultConfigString := `{"rtsp://Administrator:Password@localhost:8554/drone_0":{"amazon_kinesis_video_streams":{"aws_access_key_id":"","aws_secret_access_key":"","aws_default_region":"","kinesis_stream":""}}}`
+		// Note Change the deafult config string to json encoding.
 		// Write a config file in the current directory if it doesn't exist
-		writeToFile(applicationConfigFile, []byte(``))
+		writeToFile(applicationConfigFile, []byte(defaultConfigString))
 	}
 	// Check if the config file has not been modified
 	if sha256OfFile(applicationConfigFile) == "d41d8cd98f00b204e9800998ecf8427e" {
@@ -21,4 +24,7 @@ func init() {
 		exitTheApplication("Git is not installed in your system. Please install it and try again.")
 	}
 	// Check if the config has the correct format and all the info is correct.
+	if !jsonValid(applicationConfigFile) {
+		exitTheApplication("The config file is not a valid json file")
+	}
 }
