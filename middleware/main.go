f1f1790a91c5feb5ef1f0a06cf2e96a6c6ece562
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-03-30 14:06:43 -0400

    Update main.go

--- middleware/main.go
+++ middleware/main.go
@@ -21,7 +21,10 @@ func main() {
 				log.Println("The rtsp server is alive and we are about to start the upload.")
 				// Add a 1 to the counter
 				rtspConnectionCounter = rtspConnectionCounter + 1
-				// Start the upload
+				// Upload the feed into Google cloud vertex AI
+				forwardDataToGoogleCloudVertexAI(TestJSONValue.Num0.Host, TestJSONValue.Num0.GoogleCloudVertexAiVision.ProjectName, TestJSONValue.Num0.GoogleCloudVertexAiVision.GcpRegion, TestJSONValue.Num0.GoogleCloudVertexAiVision.VertexStreams)
+				// Upload the feed into AWS Kinesis Video Streams
+				runGstPipeline(TestJSONValue.Num0.Host, TestJSONValue.Num0.AmazonKinesisVideoStreams.KinesisStream, TestJSONValue.Num0.AmazonKinesisVideoStreams.AwsAccessKeyID, TestJSONValue.Num0.AmazonKinesisVideoStreams.AwsSecretAccessKey, TestJSONValue.Num0.AmazonKinesisVideoStreams.AwsDefaultRegion)
 				// Docs: Upload mock test data.
 				log.Println("We are about to remove a 1 from the counter.")
 				// Remove a 1 from the counter when the upload is done
