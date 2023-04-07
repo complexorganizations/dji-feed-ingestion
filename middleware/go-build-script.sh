165cd86ad0b172244728585528b73d43f3bf6dca
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-06 23:27:14 -0400

    Update go-build-script.sh

--- middleware/go-build-script.sh
+++ middleware/go-build-script.sh
@@ -5,18 +5,18 @@ function build-golang-app() {
     VERSION="v0.0.1"
     SOURCE_CODE="."
     BIN="../binaries/"
-    GOOS=linux GOARCH=386 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-386 ${SOURCE_CODE}
-    GOOS=linux GOARCH=amd64 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-amd64 ${SOURCE_CODE}
-    GOOS=linux GOARCH=arm go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm ${SOURCE_CODE}
-        GOOS=linux GOARCH=arm64 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm64 ${SOURCE_CODE}
-        GOOS=linux GOARCH=mips go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips ${SOURCE_CODE}
-        GOOS=linux GOARCH=mips64 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64 ${SOURCE_CODE}
-        GOOS=linux GOARCH=mips64le go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64le ${SOURCE_CODE}
-        GOOS=linux GOARCH=mipsle go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mipsle ${SOURCE_CODE}
-        GOOS=linux GOARCH=ppc64 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64 ${SOURCE_CODE}
-        GOOS=linux GOARCH=ppc64le go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64le ${SOURCE_CODE}
-        GOOS=linux GOARCH=riscv64 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-riscv64 ${SOURCE_CODE}
-        GOOS=linux GOARCH=s390x go build -o ${BIN}${APPLICATION}-${VERSION}-linux-s390x ${SOURCE_CODE}
+    GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-386 ${SOURCE_CODE}
+    GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-amd64 ${SOURCE_CODE}
+    GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm ${SOURCE_CODE}
+    GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm64 ${SOURCE_CODE}
+    GOOS=linux GOARCH=mips CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips ${SOURCE_CODE}
+    GOOS=linux GOARCH=mips64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64 ${SOURCE_CODE}
+    GOOS=linux GOARCH=mips64le CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64le ${SOURCE_CODE}
+    GOOS=linux GOARCH=mipsle CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mipsle ${SOURCE_CODE}
+    GOOS=linux GOARCH=ppc64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64 ${SOURCE_CODE}
+    GOOS=linux GOARCH=ppc64le CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64le ${SOURCE_CODE}
+    GOOS=linux GOARCH=riscv64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-riscv64 ${SOURCE_CODE}
+    GOOS=linux GOARCH=s390x CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-s390x ${SOURCE_CODE}
 }
 
 build-golang-app
