4d30237408089566a6cad5a6bae2d68c906f8438
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-08 11:30:59 -0400

    Update go-build-script.sh

--- middleware/go-build-script.sh
+++ middleware/go-build-script.sh
@@ -4,7 +4,7 @@ function build-golang-app() {
     APPLICATION="csp-connector"
     VERSION="v0.0.1"
     SOURCE_CODE="."
-    BIN="../binaries/"
+    BIN="binaries/"
     GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-386 ${SOURCE_CODE}
     GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-amd64 ${SOURCE_CODE}
     GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm ${SOURCE_CODE}
