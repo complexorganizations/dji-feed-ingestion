3f48c96ae08abbcd5fec67a054ea37f6e6e97b3b
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-08 11:39:31 -0400

    Update go-build-script.sh

--- middleware/go-build-script.sh
+++ middleware/go-build-script.sh
@@ -1,10 +1,14 @@
-# Build for all the OS
+# Define a function called build-golang-app
 function build-golang-app() {
+    # Navigate to the middleware directory
     cd middleware/
+    # Set values for the application, version, source code and binaries
     APPLICATION="csp-connector"
     VERSION="v0.0.1"
     SOURCE_CODE="."
     BIN="binaries/"
+    # Build the application for various OS and architectures using the go build command
+    # Set the OS and architecture values for each build, and output the resulting binary to the BIN directory with the appropriate name
     GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-386 ${SOURCE_CODE}
     GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-amd64 ${SOURCE_CODE}
     GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm ${SOURCE_CODE}
@@ -18,5 +22,5 @@ function build-golang-app() {
     GOOS=linux GOARCH=riscv64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-riscv64 ${SOURCE_CODE}
     GOOS=linux GOARCH=s390x CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-s390x ${SOURCE_CODE}
 }
-
+# Call the build-golang-app function to execute it
 build-golang-app
