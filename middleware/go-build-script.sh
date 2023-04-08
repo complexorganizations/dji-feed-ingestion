b4ff79e74050e289cd18e107b0b367c13362b540
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-08 11:42:01 -0400

    Update go-build-script.sh

--- middleware/go-build-script.sh
+++ middleware/go-build-script.sh
@@ -1,26 +1,40 @@
-# Define a function called build-golang-app
+# Build for all the OS
 function build-golang-app() {
-    # Navigate to the middleware directory
-    cd middleware/
-    # Set values for the application, version, source code and binaries
+    cd middleware/ 
+    # Change directory to middleware
     APPLICATION="csp-connector"
+    # Set application name
     VERSION="v0.0.1"
+    # Set version number
     SOURCE_CODE="."
+    # Set source code directory
     BIN="binaries/"
-    # Build the application for various OS and architectures using the go build command
-    # Set the OS and architecture values for each build, and output the resulting binary to the BIN directory with the appropriate name
+    # Set binaries directory
+    # Build for linux 386 architecture
     GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-386 ${SOURCE_CODE}
+    # Build for linux amd64 architecture
     GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-amd64 ${SOURCE_CODE}
+    # Build for linux arm architecture
     GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm ${SOURCE_CODE}
+    # Build for linux arm64 architecture
     GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-arm64 ${SOURCE_CODE}
+    # Build for linux mips architecture
     GOOS=linux GOARCH=mips CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips ${SOURCE_CODE}
+    # Build for linux mips64 architecture
     GOOS=linux GOARCH=mips64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64 ${SOURCE_CODE}
+    # Build for linux mips64le architecture
     GOOS=linux GOARCH=mips64le CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mips64le ${SOURCE_CODE}
+    # Build for linux mipsle architecture
     GOOS=linux GOARCH=mipsle CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-mipsle ${SOURCE_CODE}
+    # Build for linux ppc64 architecture
     GOOS=linux GOARCH=ppc64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64 ${SOURCE_CODE}
+    # Build for linux ppc64le architecture
     GOOS=linux GOARCH=ppc64le CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-ppc64le ${SOURCE_CODE}
+    # Build for linux riscv64 architecture
     GOOS=linux GOARCH=riscv64 CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-riscv64 ${SOURCE_CODE}
+    # Build for linux s390x architecture
     GOOS=linux GOARCH=s390x CGO_ENABLED=0 go build -o ${BIN}${APPLICATION}-${VERSION}-linux-s390x ${SOURCE_CODE}
 }
-# Call the build-golang-app function to execute it
-build-golang-app
+
+build-golang-app 
+# Call the function to execute the build process.
