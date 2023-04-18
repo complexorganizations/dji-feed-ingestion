525ec188719d4ea1cf17b5cb3a88b4fb9a80e010
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-18 09:19:04 -0400

    Update main.go

--- post-production/main.go
+++ post-production/main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func init() {
-	//
+	// Check if the SD is there.
+	// IF the sd card is there than; check if the files are there.
+	// remove the .lrf files
+	// upload the mp4 files with the srt files to s3
 }
 
 func main() {
