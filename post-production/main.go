aa7585becf3597788d9d9a635e37d51f8fef9e6e
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-18 21:54:03 -0400

    Update main.go

--- post-production/main.go
+++ post-production/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 // Check if there is a SD card connected.
@@ -16,56 +17,64 @@ func main() {
 	mountPoint := getUSBMountPoint()
 	// Get the file path
 	filePath := mountPoint + "/"
-	// Check if the directory exists
-	if !directoryExists(filePath) {
-		log.Fatalln("SD card is not connected")
-	}
-	// Check if the directory is empty
-	if isDirectoryEmpty(filePath) {
-		log.Fatalln("SD card is empty")
-	}
-	// Get all files in the directory
-	getAllFiles := walkAndAppendPath(filePath)
-	// Get all directories in the directory
-	getAllDirectories := walkAndAppendDirectory(filePath)
-	// Name all files in the directory
-	for _, file := range getAllFiles {
-		// Get the file extension
-		fileExtension := getFileExtension(file)
-		// Remove all files that are not MP4 or SRT
-		if fileExtension != ".MP4" && fileExtension != ".SRT" {
-			removeFile(file)
+
+	for {
+		// Check if the SD card is connected
+		if directoryExists(filePath) {
+			// Check if the SD card is empty
+			if !isDirectoryEmpty(filePath) {
+				// Get all files in the directory
+				getAllFiles := walkAndAppendPath(filePath)
+				// Get all directories in the directory
+				getAllDirectories := walkAndAppendDirectory(filePath)
+				// Name all files in the directory
+				for _, file := range getAllFiles {
+					// Get the file extension
+					fileExtension := getFileExtension(file)
+					// Remove all files that are not MP4 or SRT
+					if fileExtension != ".MP4" && fileExtension != ".SRT" {
+						removeFile(file)
+					}
+				}
+				// Name all directories in the directory
+				for _, directory := range getAllDirectories {
+					// Remove all the empty directories
+					if isDirectoryEmpty(directory) {
+						removeDirectory(directory)
+					}
+				}
+				// Move all the files from the SD card to the local storage
+				newLocation := getCurrentWorkingDirectory() + generateRandomString(10) + "/"
+				if !directoryExists(newLocation) {
+					createDirectory(newLocation, 0755)
+				}
+				// Move all the files from the SD card to the local storage, in a new directory with the date and time.
+				for _, file := range getAllFiles {
+					// Get the file extension
+					fileExtension := getFileExtension(file)
+					if fileExtension == ".MP4" || fileExtension == ".SRT" {
+						moveFile(file, newLocation)
+					}
+					log.Println("File:", file)
+				}
+				// Format the SD card
+				if directoryExists(filePath) {
+					if !isDirectoryEmpty(filePath) {
+						nukeDirectory(filePath)
+					}
+				}
+				// Validate if the SD card is empty
+				if isDirectoryEmpty(filePath) {
+					log.Println("SD card is empty")
+				}
+
+			} else {
+				log.Println("SD card is empty")
+			}
+		} else {
+			log.Println("SD card is connected")
 		}
-	}
-	// Name all directories in the directory
-	for _, directory := range getAllDirectories {
-		// Remove all the empty directories
-		if isDirectoryEmpty(directory) {
-			removeDirectory(directory)
-		}
-	}
-	// Move all the files from the SD card to the local storage
-	newLocation := getCurrentWorkingDirectory() + generateRandomString(10) + "/"
-	if !directoryExists(newLocation) {
-		createDirectory(newLocation, 0755)
-	}
-	// Move all the files from the SD card to the local storage, in a new directory with the date and time.
-	for _, file := range getAllFiles {
-		// Get the file extension
-		fileExtension := getFileExtension(file)
-		if fileExtension == ".MP4" || fileExtension == ".SRT" {
-			moveFile(file, newLocation)
-		}
-		log.Println("File:", file)
-	}
-	// Format the SD card
-	if directoryExists(filePath) {
-		if !isDirectoryEmpty(filePath) {
-			nukeDirectory(filePath)
-		}
-	}
-	// Validate if the SD card is empty
-	if isDirectoryEmpty(filePath) {
-		log.Println("SD card is empty")
+		// Wait 5 seconds before checking again
+		time.Sleep(5 * time.Second)
 	}
 }
