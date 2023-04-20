a51ff434aed4153fcd5c7e62e5ce635fe00f010e
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-20 15:25:35 +0000

    small change

--- middleware/helper_functions.go
+++ middleware/helper_functions.go
@@ -376,12 +376,11 @@ Imports the "os" package which provides the UserHomeDir() function
 Defines the currentUserHomeDir() function
 Invokes the UserHomeDir() function
 Returns the home directory of the current user
-Returns -1 if no user home directory is found
 */
 func currentUserHomeDir() string {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return "-1"
+		log.Println(err)
 	}
 	return homeDir
 }
