61a03044af4c579041ad4d4d9faddf98f9bce9b7
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2023-04-20 12:58:17 -0400

    Update main.go

--- middleware/main.go
+++ middleware/main.go
@@ -168,7 +168,7 @@ func init() {
 	// DEBUG: Print the Hash of the file to change it below.
 	// log.Println(sha256OfFile(applicationConfigFile))
 	// Hash the file and get the SHA-256 and make sure its not the deafult config.
-	if sha256OfFile(applicationConfigFile) == "273dfdef0f9b697b5b76f23e23e17563c9ab56eff100093b5ac1ef411546da15e19c0aae8153c64691a4a86b5db2465bebd6943b863531149b4995a3f55ba0ad" {
+	if sha256OfFile(applicationConfigFile) == "71e2cc7d4a64fe52b81d51b9f776c2fcd0e725d98ffbf4c3d2fb97c89f929f85e91db81048a00cbe5a31615ab5b319caf00702f3d6dc6ad29da9e097e71270f9" {
 		// The file has not been modified
 		saveAllErrors("Error: The config file has not been modified, Please modify it and try again.")
 	}
